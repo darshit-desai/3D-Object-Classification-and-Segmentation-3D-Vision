6035a330bc8c24f9f93e71063d7be254b79ce80d
darshit-desai <desai0681@gmail.com>
2023-11-21 11:57:44 -0500

    Added rotation test results on segmentation model

---
+++ run_seg_rot.sh
@@ -0,0 +1,26 @@
+python eval_seg.py --RotationXYZ 150.0 150.0 150.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationXYZ 180.0 180.0 180.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationXYZ 90.0 90.0 90.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationXYZ 45.0 45.0 45.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationXYZ 30.0 30.0 30.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationXYZ 15.0 15.0 15.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationX 15.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationX 30.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationX 45.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationX 90.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationX 120.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationX 150.0 --load_checkpoint best_model
+wait
+python eval_seg.py --RotationX 180.0 --load_checkpoint best_model
+wait
